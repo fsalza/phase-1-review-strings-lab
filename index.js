168d3df23602fa72694826a6122149ffd092ffb8
Fulvio Salza <fsalza96@gmail.com>
2023-03-14 16:31:08 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Fulvio Salza'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
